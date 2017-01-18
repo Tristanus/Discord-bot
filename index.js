b105ed092b6fab6964cf44172a67244940d8bff0
Tristan\Tristanus <tristan_98@hotmail.nl>
2017-01-18 12:13:54 +0100

    added add and say feature. Also installed commando plugin. Not using it atm

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-bot.on("message", (message) => {
+const prefix = "!";
 
-    if(message.content == "hoang"){
+bot.on("message", (message) => {
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+
+    let args = message.content.split(" ").slice(1);
+
+    if(command === "say"){
+        message.channel.sendMessage(args.join(" "));
+    }
+
+    if(command === "add"){
+        let numArray = args.map(n=> parseInt(n));
+        let total = numArray.reduce( (p, c) => p+c);
+
+        message.channel.sendMessage(total);
+    }
+
+    if(command === "hoang"){
         message.channel.sendMessage("Loempiavouwer");
     }
 
-    else if(message.content == "david"){
+    else if(command === "david"){
         message.channel.sendMessage("Embrace the weeb");
     }
 
-    else if(message.content == "tycho"){
+    else if(command === "tycho"){
         message.channel.sendMessage("YO!");
     }
 
-    else if(message.content == "trung"){
+    else if(command === "trung"){
         message.channel.sendMessage("u gay?");
     }
 
-    else if(message.content == "timothy"){
+    else if(command === "tim"){
         message.channel.sendMessage("Gijsbert");
     }
-
 });
 
 bot.login("MjcwOTI1NDEzMTEzNTI4MzIx.C1--jg.f-KTIsZ66dboivNX-oFF3F_cZfg");
