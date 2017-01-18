b555aa98e2778cb3f49423421d60cf1af4dffaa3
Tristan\Tristanus <tristan_98@hotmail.nl>
2017-01-18 09:16:11 +0100

    added simple text commands

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 const Discord = require("discord.js");
-const bot = new Discord.client();
+const bot = new Discord.Client();
+
+bot.on("message", (message) => {
+
+    if(message.content == "hoang"){
+        message.channel.sendMessage("Loempiavouwer");
+    }
+
+    else if(message.content == "david"){
+        message.channel.sendMessage("Embrace the weeb");
+    }
+
+    else if(message.content == "tycho"){
+        message.channel.sendMessage("YO!");
+    }
+
+    else if(message.content == "trung"){
+        message.channel.sendMessage("u gay?");
+    }
+
+    else if(message.content == "timothy"){
+        message.channel.sendMessage("Gijsbert");
+    }
+
+});
 
 bot.login("MjcwOTI1NDEzMTEzNTI4MzIx.C1--jg.f-KTIsZ66dboivNX-oFF3F_cZfg");
